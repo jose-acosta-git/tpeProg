91d8c0e24cdb45f27823cd8aa704feeb43df988a
jose-acosta-git <jose.acosta.git@gmail.com>
2022-06-20 19:53:19 -0300

    refactor index: binary search implemented

--- first-part/src/Index.java
+++ first-part/src/Index.java
@@ -1,45 +1,65 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Index {
 
-  private LinkedList<Genre> books;
+  private ArrayList<Genre> books;
 
   public Index() {
-    books = new LinkedList<Genre>();
+    books = new ArrayList<Genre>();
   }
 
   public void addBook(String genre, Book book) {
-    boolean found = false;
-    Iterator<Genre> it = this.books.iterator();
-
-    while (it.hasNext() && !found) {
-      Genre current = it.next();
-      if (current.getName().equals(genre)) {
-        found = true;
-        current.addBook(book);
-      }
-    }
-
-    if (!found) {
+    if (books.isEmpty()) {
       Genre newGenre = new Genre(genre);
       newGenre.addBook(book);
       this.books.add(newGenre);
+    } else {
+      addBookOrdered(genre, book, 0, books.size()-1);
     }
+  }
 
+  public void addBookOrdered(String genre, Book book, int start, int end) {
+    int mid = 1;
+    if (end < start) {
+      Genre newGenre = new Genre(genre);
+      newGenre.addBook(book);
+      this.books.add(start, newGenre);
+    } else {
+      mid = (start + end) / 2;
+      if ( genre.compareTo(books.get(mid).getName()) > 0 ) {
+        addBookOrdered(genre, book, mid+1, end);
+      } else {
+        if ( genre.compareTo(books.get(mid).getName()) < 0 ) {
+          addBookOrdered(genre, book, start, mid-1);
+        } else {
+          books.get(mid).addBook(book);
+        }
+      }
+    }
   }
 
   public LinkedList<Book> getByGenre(String genre) {
-    Iterator<Genre> it = this.books.iterator();
+    return search(genre, 0, books.size()-1);
+  }
 
-    while (it.hasNext()) {
-      Genre current = it.next();
-      if (current.getName().equals(genre)) {
-        return current.getBooks();
+  public LinkedList<Book> search(String search, int start, int end) {
+    int mid = 1;
+    if (end < start) {
+      return new LinkedList<>();
+    } else {
+      mid = (start+end) / 2;
+      if (search.compareTo(books.get(mid).getName()) > 0) {
+        return search(search, mid+1, end);
+      } else {
+        if (search.compareTo(books.get(mid).getName()) < 0) {
+          return search(search, start, mid-1);
+        } else {
+          return books.get(mid).getBooks();
+        }
       }
     }
-
-    return new LinkedList<Book>();
   }
 
 }
--- first-part/src/Main.java
+++ first-part/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.text.Normalizer;
 import java.util.LinkedList;
 
 public class Main {
@@ -27,8 +26,10 @@ public class Main {
     do {
       try {
         System.out.print("Ingrese un género para realizar la búsqueda: ");
+        /* normalizacion
         genre = Normalizer.normalize(input.readLine(), Normalizer.Form.NFD);
-        genre = genre.replaceAll("[^a-z,^A-Z,^0-9]", "");
+        genre = genre.replaceAll("[^a-z,^A-Z,^0-9]", "");*/
+        genre = input.readLine();
         error = false;
       } catch (Exception e) {
         System.out.println("Ingreso inválido: " + e);
